f2ea9044b162e02a35918c360b72e2707a4b7d10
Steve Copley <steve.copley@waimea.school.nz>
2025-02-07 14:45:36 +1300

    Examples of class and objects

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,87 @@
 fun main() {
     val cat1 = Cat("Jimmy")
-    val cat2 = Cat("Dave")
+    val cat2 = Cat("Dave", 1)
+    val cat3 = Cat("Nigel")
 
     println(cat1)
     println(cat2)
+    println(cat3)
 
+    cat1.poke()
+    cat1.stroke()
+    cat1.stroke()
+    cat1.poke()
+
+    println(cat1)
+    cat1.makeFriends(cat3)
+    println(cat1)
+    println(cat3)
 }
 
-class Cat(val name: String)
+class Cat(val name: String, var legs: Int = 4) {
+    var isSleeping: Boolean = false;
+    var isHungry: Boolean = false;
+    var bff: Cat? = null
+
+    // Method that updates state
+    fun poke() {
+        if (isSleeping) {
+            isSleeping = false
+            println("You woke up $name!")
+        }
+        else {
+            println("$name is angry and scratches you")
+        }
+    }
+
+    // Method that updates state
+    fun stroke() {
+        println("$name purrrrrrrrrrs...")
+
+        if (!isSleeping) {
+            isSleeping = true
+            println("$name falls asleep")
+        }
+    }
+
+    // Method where data is supplied
+    fun makeFriends(newFriend: Cat) {
+        bff = newFriend         // Connect to them from us
+        newFriend.bff = this    // Connect back to us ('this')
+
+        println("$name is now friends with ${newFriend.name}")
+    }
+
+
+
+
+
+
+    // This method is run when an object is created
+    init {
+        println("Creating a new cat")
+    }
+
+    // This replaces the default function that prints
+    // the object's ID with something more useful
+    override fun toString(): String {
+        var info: String = ""
+
+        info += "Name: $name\n"
+        info += "Legs: $legs\n"
+        info += "Hungry: $isHungry\n"
+        info += "Sleeping: $isSleeping\n"
+        if (bff != null) {
+            info += "Bff: ${bff!!.name}"
+        }
+        else {
+            info += "No BFF!"
+        }
+
+
+        return info
+    }
+}
 
 
 
